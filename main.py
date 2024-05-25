b1ce06be51955c2a593e35fabdbd9ef51848922d
Nelson Malbone <nhmalbone@icloud.com>
2024-05-25 12:50:18 -0400

    app8 Book_Analysis integrated a user input so the user can type in a word and see how many times that word is used

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import regex as re
 #1 read the file
 
-with open('miracle_in_the_andes.txt', 'r') as file:
-    content = file.read()
+with open('miracle_in_the_andes.txt', 'r', encoding="UTF-8") as file:
+    book = file.read()
 
+def find(w):
+    patterns = re.compile("[a-zA-Z]+")
+    findings = re.findall(patterns, book.lower())
+    d = {}
+
+    for word in findings:
+        if word in d.keys():
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+    try:
+        return d[w]
+    except:
+        return f'The book does not contain the word {w}'
+
+w = input("see how many times a word is used or said: ")
+
+finder = find(w)
+
+print(finder)
 

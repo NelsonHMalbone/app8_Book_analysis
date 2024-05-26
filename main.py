0908363e8e9059d474ce2f00ef6f53da2c2bd462
Nelson Malbone <nhmalbone@icloud.com>
2024-05-25 20:34:56 -0400

    app8 Book_Analysis added a error out so when you exit the program it dont throw errors and also added so you can keep typing in words with out having to rerun program

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ import regex as re
 
 with open('miracle_in_the_andes.txt', 'r', encoding="UTF-8") as file:
     book = file.read()
+try:
+    while True:
+        def find(w):
+            patterns = re.compile("[a-zA-Z]+")
+            findings = re.findall(patterns, book.lower())
+            d = {}
+
+            for word in findings:
+                if word in d.keys():
+                    d[word] = d[word] + 1
+                else:
+                    d[word] = 1
+            try:
+                return d[w]
+            except:
+                return f'The book does not contain the word {w}'
 
 
-def find(w):
-    patterns = re.compile("[a-zA-Z]+")
-    findings = re.findall(patterns, book.lower())
-    d = {}
-
-    for word in findings:
-        if word in d.keys():
-            d[word] = d[word] + 1
-        else:
-            d[word] = 1
-    try:
-        return d[w]
-    except:
-        return f'The book does not contain the word {w}'
 
 
-w = input("see how many times a word is used or said: ")
 
-finder = find(w)
+        w = input("see how many times a word is used or said: ")
 
-print(finder)
+        finder = find(w)
 
+        print(finder)
+
+except KeyboardInterrupt:
+    print("goodbye")
